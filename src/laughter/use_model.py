52c58a3602443accb37f62e5a4abf71deae95283
hannahshader <38895141+hannahshader@users.noreply.github.com>
2023-11-08 14:56:21 -0500

    Setting up

---
+++ src/laughter/use_model.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: Hannah Shader
+# @Date:   2023-11-05 11:19:14
+# @Last Modified by:   Hannah Shader
+# @Last Modified time: 2023-11-05 12:42:38
+from laughter_detection.use_model_internal import run_module
+
+run_module("anything")
